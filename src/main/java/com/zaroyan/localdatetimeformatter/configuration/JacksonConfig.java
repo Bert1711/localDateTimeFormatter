29e66f9f2e5040e65b2658067beda7f0d2a8e9df
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-02 12:12:44 +0300

    "Добавлен локаль, по дефолту"

--- src/main/java/com/zaroyan/localdatetimeformatter/configuration/JacksonConfig.java
+++ src/main/java/com/zaroyan/localdatetimeformatter/configuration/JacksonConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 /**
  * @author Zaroyan
@@ -17,7 +18,9 @@ public class JacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
         return builder -> {
-            builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy:MM:dd | HH:mm:ss:SSS")));
+            builder.serializerByType
+                    (LocalDateTime.class, new LocalDateTimeSerializer
+                            (DateTimeFormatter.ofPattern("yyyy:MM:dd | HH:mm:ss:SSS",  Locale.getDefault())));
         };
     }
 }
